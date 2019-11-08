f41f71bfe95e204c0c88891504e159aa64f6dd2f
StephenThajeb <sthajeb1@gmail.com>
2019-11-08 20:48:46 +0700

    edit map

--- map.pl
+++ map.pl
@@ -12,10 +12,9 @@ activeArea(X,Y) :- X>(0),X<11,Y>(0),Y<11.
 
 /* Inisialisasi awal */
 init :- asserta(playerLoc(1,1)).
-init :- asserta(friendLoc(7,9)).
-init :- asserta(enemyLoc(11,11)).
+init :- asserta(friendLoc(2,2)).
+init :- asserta(enemyLoc(10,10)).
 init :- asserta(healLoc(6,10)).
-
 
 printmap(X,Y) :- playerLoc(X,Y),write('P').
 printmap(X,Y) :- border(X,Y),write('X').
@@ -24,19 +23,25 @@ printmap(X,Y) :- enemyLoc(X,Y),write('E').
 printmap(X,Y) :- healLoc(X,Y),write('H').
 printmap(X,Y) :- activeArea(X,Y),write('-').
 
-
 /*Move*/
-invalidMove :- write('Invalid Move !! You just headed for the wall'),nl.
+invalidMove :- write('Invalid Move !! Kamu tidak boleh melewat border cuy'),nl.
 
-east  :- playerLoc(11,_),invalidMove,!.
-east  :- playerLoc(X,Y),NewX is X+1,retract(playerLoc(X,Y)),asserta(playerLoc(NewX,Y)),!.
+east  :- playerLoc(10,_),invalidMove,!.
+east  :- playerLoc(X,Y),NewX is X+1,retract(playerLoc(X,Y)),asserta(playerLoc(NewX,Y)),msgAfterMove.
 west  :- playerLoc(1,_),invalidMove,!.
-west  :- playerLoc(X,Y),NewX is X-1,retract(playerLoc(X,Y)),asserta(playerLoc(NewX,Y)),!.
-north  :- playerLoc(_,1),invalidMove,!.
-north :- playerLoc(X,Y),NewY is Y+1,retract(playerLoc(X,Y)),asserta(playerLoc(X,NewY)),!.
-south  :- playerLoc(_,11),invalidMove,!.
-south :- playerLoc(X,Y),NewY is Y-1,retract(playerLoc(X,Y)),asserta(playerLoc(X,NewY)),!.
+west  :- playerLoc(X,Y),NewX is X-1,retract(playerLoc(X,Y)),asserta(playerLoc(NewX,Y)),msgAfterMove.
+north :- playerLoc(_,1),invalidMove,!.
+north :- playerLoc(X,Y),NewY is Y-1,retract(playerLoc(X,Y)),asserta(playerLoc(X,NewY)),msgAfterMove.
+south :- playerLoc(_,10),invalidMove,!.
+south :- playerLoc(X,Y),NewY is Y+1,retract(playerLoc(X,Y)),asserta(playerLoc(X,NewY)),msgAfterMove.
 
-/*east :- currLoc(X,Y), Y1 is Y+1, deadzone(X,Y1), !, retract(currLoc(X,Y)), asserta(currLoc(X,Y1)), minushp, !.*/
+/*Logic ketika player ketemu friend, enemy, atau berada di health center */
+msgAfterMove :- playerLoc(X,Y),write('Sekarang anda berada pada ('),print(X),write(','),print(Y),write(')'),!.
+
+/*
+msgAfterMove :- playerLoc(X,Y),friendLoc(X,Y),write('Kamu ketemu dengan ..... teman baik lamamu,coba meyakinkan dia untuk mengikuti kamu atau hindarin dia?').
+msgAfterMove :- playerLoc(X,Y),enemyLoc(X,Y),write('Kamu ketemu dengan musuh legendary Clan Akatsuki, lari atau bertanding ?'),!.
+msgAfterMove :- playerLoc(X,Y),healthLoc(X,Y),write('Anda berada di Health Center,Anda boleh menyembuhkan 1 orang di sini, Menyembuhkan siapa ?'),!.
+*/
 
 map:- forall(between(0,11,Y),(forall(between(0,11,X),printmap(X,Y)),nl)).
