ae5a3fd7e16183075c163b82b09a08ef6baf2c0c
Daffa Pratama <daffapratama8c29@gmail.com>
2019-11-09 21:05:20 +0700

    battle text

--- print.pl
+++ print.pl
@@ -34,7 +34,10 @@ print_Win :-
     write('  Y     O     O    U    U         W  W W  W   II  N   N N'),nl,
     write('  Y      OOOOO      UUUU           WW   WW    II  N    NN'),nl,
     write('========================================================='),nl,
-    write('=====================HINATA IS SAFE======================'),nl.
+    write('=====================HINATA IS SAFE======================'),nl,
+    write('Selamat kau berhasil menyelamatkan Hinata dan membawanya pulang.'),nl,
+    write('Tetaplah berlatih untuk menjadi ninja yang hebat dan jangan lupakan'),nl,
+    write('teman-temanmu!'),nl.
 
 print_Lose :-
     write('Y   Y    OOOOO    U      U       L       OOOOO     SSSSS  EEEEE'),nl,
@@ -43,10 +46,59 @@ print_Lose :-
     write('  Y     O     O    U    U        L      O     O        S  E'),nl,
     write('  Y      OOOOO      UUUU         LLLLL   OOOOO    SSSSS   EEEEE'),nl,
     write('==============================================================='),nl,
-    write('====================BETTER LUCK NEXT TIME======================'),nl.
+    write('====================BETTER LUCK NEXT TIME======================'),nl,
+    write('Sayang sekali misimu telah gagal, namun tekad dalam dirimu akan tetap'),nl,
+    write('ada dan terus ada sampai Hinata kembali pulang.'),nl.
 
 print_all :-
     print_Title,
     print_Story,
     print_Win,
     print_Lose.
+
+print_InventoryFull :-
+    write('Maafkan aku tidak bisa ikut denganmu mencari Hinata. Semoga kau bisa menyelamatkannya!'),nl.
+print_InventoryNotFull :-
+    write('Ayo kita cari Hinata dan bawa ia pulang ke Konoha!'), nl.
+print_Run :-
+    write('Kagebunshin no Jutsu!'),nl,
+    write('Berhasil kabur!'),nl.
+print_CantRun :-
+    write('Kagebunshin no Jutsu!'),nl,
+    write('..........'),nl,
+    write('Sial! Aku tidak bisa kabur!'),nl.
+
+print_FightSakura :-
+    write('Haii. Jangan nangis ya kalau kalah...'),nl.
+print_FightSasuke :-
+    write('Hahahaha, teman lama. Sudah sekuat apa kau sekarang?'),nl.
+print_FightLee :-
+    write('Akan aku buktikan hasil latihanku dengan Guru Guy!'),nl.
+print_FightNeji :-
+    write('Hinata.... Lawan aku dulu sebelum kau selamatkan Hinata'),nl.
+print_FightChoji :-
+    write('Nyam nyam nyam... Lewati aku kalau bisa'),nl.
+print_FightShikamaru :-
+    write('Coba hindari bayanganku kalau bisa!'),nl.
+print_FightTenten :-
+    write('Guru Guy tidak pernah salah melatih muridnya!'),nl.
+print_FightKiba :-
+    write('Akamaruu! Seraaaaang!'),nl.
+print_FightShino :-
+    write('Gooooo Parasitee....!'),nl.
+print_FightGaara :-
+    write('Masih berani bertarung diatas pasir, huh?!'),nl.
+print_FightDeidara :-
+    write('Ledakan adalah nama tengahku. HAHAHAHAHA'),nl.
+print_FightTobi :-
+    write('Hancurkan topengku kalah kau bisa!'),nl.
+print_FightSasori :-
+    write('Jangan pernah remehkan bonekaku. Hahahahaha....'),nl.
+print_FightItachi :-
+    write('Well well well. Semua pasukanku sudah berhasil kau taklukkan.'),nl,
+    write('Tetapi Hinata tetap akan jadi milik Akatsuki!'),nl.
+
+print_BattleTitle :-
+    write('=================================================='),nl,
+    write('================== BATTLE BEGIN =================='),nl,
+    write('=================================================='),nl.
