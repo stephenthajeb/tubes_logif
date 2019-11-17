65715c0531e880e33e841ddc5c66e12f9ea0ff12
mirzaalarsyad <mirza.fathan@gmail.com>
2019-11-17 16:42:27 +0700

    Nambah savegame, belum di implementasi

---
+++ savegame.pl
@@ -0,0 +1,39 @@
+:- include('character.pl').
+
+save:-
+	nl, write('Ingin simpan dengan nama?'), nl,
+	write('>> '), read(File),
+	atom_concat(File, '.txt', Filetxt),
+	open(Filetxt, write, Stream),
+	save_status(Stream),
+	close(Stream), 	write('Permainan telah disimpan!'), nl.
+
+save_status(Stream) :-
+	save_player(Stream).
+save_status(Stream) :-
+	save_enemy(Stream).
+save_status(Stream) :-
+	save_inventory(Stream).
+save_status(Stream) :-
+	save_currHP(Stream).
+save_status(_) :- !.
+
+save_player(Stream) :-
+	player(Name, Type, X, Y, HP, NDamage, SDamage),
+	write(Stream, player(Name, Type, X, Y, HP, NDamage, SDamage)), write(Stream, '.'), nl(Stream),
+	fail.
+
+save_enemy(Stream) :-
+ 	enemy(Name, Type, X, Y, HP, NDamage, SDamage),
+	write(Stream, enemy(Name, Type, X, Y, HP, NDamage, SDamage)), write(Stream, '.'), nl(Stream),
+	fail.
+
+save_inventory(Stream) :-
+	inventory(ListInventory),
+	write(Stream, inventory(ListInventory)),  write(Stream, '.'), nl(Stream),
+	fail.
+
+save_currHP(Stream) :-
+	currHP(ListHP),
+	write(Stream, currHP(ListHP)), write(Stream, '.'), nl(Stream),
+	fail.
