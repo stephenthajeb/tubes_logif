535e2ad8724c3bffbb554a67464e1dae8a1a7124
mirzaalarsyad <mirza.fathan@gmail.com>
2019-11-17 17:37:08 +0700

    update menu help di print.pl

--- print.pl
+++ print.pl
@@ -25,7 +25,7 @@ print_Story :-
     write('Mengapa teman-teman Naruto malah mengajak ia bertarung ?'),nl,
     write('Berhasilkah Naruto menaklukan Akatsuki dan membawa Hinata pulang ?'),nl,
     nl,
-    write('Let the game begin !'),nl, nl.
+    write('Let the game begin !'),nl.
     
 print_Win :-
     write('Y   Y    OOOOO    U      U       W         W  II  NN    N'),nl,
@@ -50,86 +50,115 @@ print_Lose :-
     write('Sayang sekali misimu telah gagal, namun tekad dalam dirimu akan tetap'),nl,
     write('ada dan terus ada sampai Hinata kembali pulang.'),nl.
 
+print_all :-
+    print_Title,
+    print_Story,
+    print_Win,
+    print_Lose.
+
 /* Interactive Command */
 
 print_InventoryFull :-
-    write('Temanmu sudah terlalu banyak. Apakah ingin mengganti teman ? (yes/no)'),nl.
+    write('Temanmu sudah terlalu banyak. Apakah ingin mengganti teman ?'),nl.
 print_Capture :-
     write('Ayo kita cari Hinata dan bawa ia pulang ke Konoha!'), nl.
 print_NotCapture :-
-    write('Misi ini sangat berbahaya,'),nl,
-    write('Aku tidak mau membahayakanmu...'),nl.
+    write('Arrgghh... kau berhasil mengalahkanku...'),nl,
+    write('Tapi aku tidak akan menyerah....'),nl.
 print_Run :-
-    nl,
-    write('Kagebunshin no Jutsu!'),nl,nl,
+    write('Kagebunshin no Jutsu!'),nl,
     write('Berhasil kabur...'),nl.
 print_CantRun :-
-    nl,
-    write('Kagebunshin no Jutsu!'),nl,nl,
-    write('. . . . . . .'),nl,nl,
+    write('Kagebunshin no Jutsu!'),nl,
+    write('..........'),nl,
     write('Sial! Aku tidak bisa kabur!'),nl.
 
 print_FoundEnemy :-
-    write('Hati-hati ada musuh didepan!'),nl, nl,
-    enemy(Name,_,_,_,_,_,_),
-    write(Name), write(' muncul secara tiba tiba'),nl.
+    write('Hati-hati ada musuh didepan!'),nl,
+    write('X'), write(' muncul secara tiba tiba'),nl,
+    write('Pilih bertarung atau lari ?'),nl.
 
-print_Fight(sakura) :-
+print_Heal :-
+    write('Akhirnya sampai di Medical Center'),nl,
+    write('Mau sembuhkan teman-teman ?'),nl.
+
+print_FightSakura :-
     write('Haii. Jangan nangis ya kalau kalah...'),nl.
-print_Fight(sasuke) :-
+print_FightSasuke :-
     write('Hahahaha, teman lama. Sudah sekuat apa kau sekarang?'),nl.
-print_Fight(lee) :-
+print_FightLee :-
     write('Akan aku buktikan hasil latihanku dengan Guru Guy!'),nl.
-print_Fight(neji) :-
+print_FightNeji :-
     write('Hinata.... Lawan aku dulu sebelum kau selamatkan Hinata'),nl.
-print_Fight(choji) :-
+print_FightChoji :-
     write('Nyam nyam nyam... Lewati aku kalau bisa'),nl.
-print_Fight(shikamaru) :-
+print_FightShikamaru :-
     write('Coba hindari bayanganku kalau bisa!'),nl.
-print_Fight(tenten) :-
+print_FightTenten :-
     write('Guru Guy tidak pernah salah melatih muridnya!'),nl.
-print_Fight(kiba) :-
+print_FightKiba :-
     write('Akamaruu! Seraaaaang!'),nl.
-print_Fight(shino) :-
+print_FightShino :-
     write('Gooooo Parasitee....!'),nl.
-print_Fight(gaara) :-
+print_FightGaara :-
     write('Masih berani bertarung diatas pasir, huh?!'),nl.
-print_Fight(deidara) :-
+print_FightDeidara :-
     write('Ledakan adalah nama tengahku. HAHAHAHAHA'),nl.
-print_Fight(tobi) :-
-    write('Hancurkan topengku kalau kau bisa!'),nl.
-print_Fight(sasori) :-
+print_FightTobi :-
+    write('Hancurkan topengku kalah kau bisa!'),nl.
+print_FightSasori :-
     write('Jangan pernah remehkan bonekaku. Hahahahaha....'),nl.
-print_Fight(itachi) :-
+print_FightItachi :-
     write('Well well well. Semua pasukanku sudah berhasil kau taklukkan.'),nl,
     write('Tetapi Hinata tetap akan jadi milik Akatsuki!'),nl.
 
 print_BattleTitle :-
     write('=================================================='),nl,
-    write('================== BATTLE BEGIN =================='),nl,
+    write('================== BATTLE BEGIN =================='),nl,                         
     write('=================================================='),nl.
 
 /* Help */
-
+/* Ini aku ganti jadi help */ 
+/*
+print_Help :-
+    write('Command-command yang tersedia'),nl,
+    write('Ketik nomor command dan akhiri dengan tanda titik untuk menggunakannya'),nl,
+    write('[ 1]Mulai Permainan'),nl,
+    write('[ 2]Barat'),nl,s
+    write('[ 3]Timur'),nl,
+    write('[ 4]Utara'),nl,
+    write('[ 5]Selatan'),nl,
+    write('[ 6]Tunjukkin arah mata angin dong'),nl,
+    write('[ 7]Attack'),nl,
+    write('[ 8]Kabur'),nl,
+    write('[ 9]Menambah anggota mission'),nl,
+    write('[10]Melepas anggota mission'),nl,
+    write('[11]Status'),nl,
+    write('[12]Perlihatkan peta'),nl,
+    write('[13]Save'),nl,
+    write('[14]Load'),nl,
+    write('[15]Help'),nl,
+    write('[16]Keluar Permainan'),nl.
+*/
 print_Help :-
     write('----------------HELP----------------'),nl,
     write('Ketik command yang tersedia dan akhiri dengan tanda titik (.)'),nl,
-    write('w        : Utara'),nl,
-    write('a        : Barat'),nl,
-    write('s        : Selatan'),nl,
-    write('d        : Timur'),nl,
-    write('battle   : Masuk '),nl,
-    write('run      : Kabur'),nl,
-    write('normal   : Menyerang musuh dengan normal attack'),nl,
-    write('skill    : Menyerang musuh dengan skill attack'),nl,
-    write('capture  : Menambah teman'),nl,
-    write('swap     : Mengganti teman'),nl,
-    write('status   : Status'),nl,
-    write('map      : Perlihatkan peta'),nl,
-    write('help     : Help'),nl,
-    write('exit     : Keluar Permainan'),nl, nl.
+    write('w    : Utara'),nl,
+    write('a    : Barat'),nl,
+    write('s    : Selatan'),nl,
+    write('d    : Timur'),nl,
+    write('attack  : Attack'),nl,
+    write('run  : Kabur'),nl,
+    write('add  : Menambah anggota mission'),nl,
+    write('del  : Melepas anggota mission'),nl,
+    write('status : Status'),nl,
+    write('map  : Perlihatkan peta'),nl,
+    write('save : Save'),nl,
+    write('load : Load'),nl,
+    write('help : Help'),nl,
+    write('exit : Keluar Permainan'),nl.
 
-status :-
+print_PlayerStatus :-
     inventory(Inventory),
     player(Name, Type, X, Y, HP, NDamage, SDamage),
     skillName(Name, SName),
@@ -141,90 +170,23 @@ status :-
     write('Skill: '), write(SName), write(' / '), write(SDamage),nl,
     write('Allies: '),write(Inventory),nl.
 
-playerstatus :-
-    player(Name, Type, _, _, HP, NDamage, SDamage),
+print_EnemyStatus :-
+    enemy(Name, Type, X, Y, HP, NDamage, SDamage),
     skillName(Name, SName),
-    write('===== PLAYER ====='),nl,
     write('Name : '), write(Name),nl,
     write('Type : '), write(Type),nl,
+    write('Loc  : '), write('('), write(X), write(','), write(Y), write(')'),nl,
     write('HP   : '), write(HP),nl,
     write('Dmg  : '), write(NDamage),nl,
     write('Skill: '), write(SName), write(' / '), write(SDamage),nl.
 
-enemystatus :-
-    enemy(Name, Type, _, _, HP, NDamage, SDamage),
-    skillName(Name, SName),
-    write('===== ENEMY ====='),nl,
-    write('Name : '), write(Name),nl,
-    write('Type : '), write(Type),nl,
-    write('HP   : '), write(HP),nl,
-    write('Dmg  : '), write(NDamage),nl,
-    write('Skill: '), write(SName), write(' / '), write(SDamage),nl.
+printCommandInvalid :- write('Input tidak valid'),nl.
 
-printCommandInvalid :- write('Command tidak valid'),nl.
+printInputCommand :- write('Masukkin commmand: ').
+
+printArah :- 
+    write('  N'),nl,
+    write('W   E'),nl,
+    write('  S'),nl.
 
 printInvalidMove :- write('Invalid Move !! Kamu tidak boleh melewat border cuy'),nl.
-
-print_Heal :-
-    healStatus(0),
-    write('Akhirnya sampai di Medical Center'),nl,
-    write('Mau heal teman-teman ? Jika mau ketik "heal."'),nl,!.
-print_Heal :-
-    healStatus(1),
-    write('keluar'),
-    write('Fasilitas ini hanya bisa digunakan sekali :('),nl,!.
-
-printSkillUsed :- write('Skill hanya bisa digunakan sekali cuy dalam battle ini'),nl.
-
-printKO :- player(PlayerName, _,_,_,_,_,_),
-           write(PlayerName),
-           write(' is defeated '),nl,
-           write('Aku terlalu lemah untuk menyelamatkan Hinata'),nl,
-           write('Silahkan assign new player dari inventory'),nl.
-
-print_Inventory([]):- !.
-print_Inventory([H|T]):-
-    write('- '),
-    print(H),nl,
-    print_Inventory(T).
-print_Invalid:-
-    write(' Masukan tidak Valid, Silahkan masukkan kembali '),nl.
-
-print_Invalid_Collision(Input):-
-    \+(Input==run),\+(Input==attack),
-    print_Invalid,!.
-
-print_Invalid_Collision(_).
-
-print_InvalidYes(Input,ListInventory):-
-    check(ListInventory,Input),
-    print_Invalid,
-    !.
-
-print_InvalidYes(_,_).
-
-print_InvalidCapture(Input):-
-    \+(Input==yes),\+(Input==no),
-    print_Invalid,
-    !.
-
-print_InvalidCapture(_).
-
-print_InvalidCapture1(Input):-
-    \+(Input==capture),\+(Input==no),
-    print_Invalid,
-    !.
-print_InvalidCapture1(_).
-
-print_InvalidAttack(Input):-
-    \+(Input==normal),\+(Input==skill),
-    print_Invalid,
-    !.
-print_InvalidAttack(_).
-
-print_InvalidCommand(Input):-
-    \+(Input==w),\+(Input==a),\+(Input==s),\+(Input==d),\+(Input==status),\+(Input==map),\+(Input==help),\+(Input==exit),
-    print_Invalid,
-    !.
-print_InvalidCommand(_).
-        
